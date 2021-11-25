22f24379f24ac231960cca698af36b6bfe172aeb
lucasew <lucas59356@gmail.com>
2021-11-24 21:27:21 -0300

    test.nix -> example.nix
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- example.nix
+++ example.nix
@@ -1,3 +1,7 @@
+# Config example used to demo if it works and as a baseline for users to experiment
+# Use `nix-build example.nix` and open emacs via ./result/bin/emacs
+# This file returns a function that returns a derivation so import/callPackage and nix-env should work as well
+
 {pkgs ? import <nixpkgs> {}}:
 pkgs.callPackage ./default.nix {
   initEl = {
