5eb232003e89edb978fe584dc5c15db7335fc183
lucasew <lucas59356@gmail.com>
2021-12-23 13:54:10 -0300

    modules/org/roam: add some default bindings
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- modules/org/org-roam.nix
+++ modules/org/org-roam.nix
@@ -13,5 +13,16 @@ in {
     initEl.pre = mkIf config.org.roam.ack-v2 ''
       (setq org-roam-v2-ack t)
     '';
+    initEl.pos = ''
+      (add-hook 'after-init-hook (lambda ()
+          (define-key (current-global-map) (kbd "C-c n f") 'org-roam-node-find)
+          (define-key (current-global-map) (kbd "C-c n r") 'org-roam-node-random)
+          (define-key (org-mode-map) (kbd "C-c n i") 'org-roam-node-insert)
+          (define-key (org-mode-map) (kbd "C-c n o") 'org-id-get-create)
+          (define-key (org-mode-map) (kbd "C-c n t") 'org-roam-tag-add)
+          (define-key (org-mode-map) (kbd "C-c n a") 'org-roam-alias-add)
+          (define-key (org-mode-map) (kbd "C-c n l") 'org-roam-buffer-toggle)))
+
+    '';
   };
 }
